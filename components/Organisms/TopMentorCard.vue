085fcc1071637b076203d9553f2721730ea947ac
tomoki inoue <>
2021-02-23 19:43:38 +0900

    complete top page layout

---
+++ components/Organisms/TopMentorCard.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374">
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate></v-progress-linear>
+    </template>
+
+    <v-img
+      height="150"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+    <v-row
+      class="ml-4 mt-4">
+      <v-avatar
+        size=60>
+        <img
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          alt="John">
+      </v-avatar>
+      <v-card-title>{{ name }}</v-card-title>
+    </v-row>    
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0">
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="18"></v-rating>
+
+        <div class="grey--text ml-4">
+          4.5 (413)
+        </div>
+
+        <div class="ml-4">
+          <button-outlined 
+            title="レビューを見る"
+            :clickBtn="reserve" />
+        </div>
+
+      </v-row>
+
+      <div class="mt-4 mb-1 subtitle-1">
+        得意スキル
+      </div>
+
+      <div>
+        <v-chip
+          color="green"
+          text-color="white">
+          営業
+        </v-chip>
+        <v-chip
+          color="red"
+          text-color="white">
+          IT
+        </v-chip>
+        <v-chip
+          color="blue"
+          text-color="white">
+          金融
+        </v-chip>
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>   
+
+    <v-card-text>
+      <div class="mb-1 subtitle-1">
+        Mentorスコア
+      </div>
+
+      <div class="title">
+        15,643
+      </div>
+    </v-card-text> 
+
+    <v-divider class="mx-4"></v-divider>  
+
+    <v-card-text>
+      <div class="mb-1 subtitle-1">
+        契約人数
+      </div>
+
+      <div class="title">
+        88人
+      </div>
+    </v-card-text> 
+
+  </v-card>
+</template>
+
+<script>
+import ButtonOutlined from '../Atoms/ButtonOutlined.vue'
+
+export default {
+  components: { ButtonOutlined },
+  props: {
+    name: {},
+    clickCard: {
+      type: Function
+    }
+  },
+  data: () => ({
+    loading: false,
+    selection: 1,
+  }),
+  methods: {
+    reserve () {
+      this.loading = true
+      setTimeout(() => (this.loading = false), 2000)
+      
+    },
+    click() {
+      this.clickCard()
+    },
+  },
+}
+</script>
---
+++ pages/top.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-carousel
+        cycle
+        width="100%"
+        height="600"
+        hide-delimiters
+        :show-arrows="false">
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src">
+        </v-carousel-item>
+      </v-carousel>
+      <v-col 
+        v-for="(item, i) in slides"
+        :key="i">
+        <MentorCard 
+        :name="item" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import MentorCard from '../components/Organisms/TopMentorCard.vue'
+
+export default {
+  components: { MentorCard },
+  data () {
+    return {
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+      ],
+      items: [
+        {
+          src: require('@/assets/images/top/top1.png'),
+        },
+        {
+          src: require('@/assets/images/top/top1.png'),
+        },
+        {
+          src: require('@/assets/images/top/top1.png'),
+        },
+        {
+          src: require('@/assets/images/top/top1.png'),
+        },
+      ],
+    }
+  },
+}
+</script>
