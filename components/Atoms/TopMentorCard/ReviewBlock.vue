3e4e05d88273327135d3af958b7c5e32010d383d
tomoki inoue <>
2021-02-26 19:57:32 +0900

    devide component

---
+++ components/Atoms/TopMentorCard/ReviewBlock.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card-text>
+    <v-row
+      align="center"
+      class="mx-0">
+      <v-rating
+        :value="review.value"
+        color="amber"
+        dense
+        half-increments
+        readonly
+        size="18"></v-rating>
+
+      <div class="grey--text ml-4">
+        {{ review.value }}
+      </div>
+
+      <div class="ml-4">
+        <button-outlined 
+          title="レビューを見る"
+          :clickBtn="reserve" />
+      </div>
+    </v-row>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  props: {
+    review: {},
+  }
+}
+</script>
--- components/Organisms/TopMentorCard.vue
+++ components/Organisms/TopMentorCard.vue
@@ -25,85 +25,34 @@
       <v-card-title>{{ name }}</v-card-title>
     </v-row>    
 
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0">
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="18"></v-rating>
+    <review-block 
+      :review="review" />
 
-        <div class="grey--text ml-4">
-          4.5 (413)
-        </div>
-
-        <div class="ml-4">
-          <button-outlined 
-            title="レビューを見る"
-            :clickBtn="reserve" />
-        </div>
-
-      </v-row>
-
-      <div class="mt-4 mb-1 subtitle-1">
-        得意スキル
-      </div>
-
-      <div>
-        <v-chip
-          color="green"
-          text-color="white">
-          営業
-        </v-chip>
-        <v-chip
-          color="red"
-          text-color="white">
-          IT
-        </v-chip>
-        <v-chip
-          color="blue"
-          text-color="white">
-          金融
-        </v-chip>
-      </div>
-    </v-card-text>
+    <card-skill-block 
+      title="得意スキル"
+      :skills="skills" />
 
     <v-divider class="mx-4"></v-divider>   
 
-    <v-card-text>
-      <div class="mb-1 subtitle-1">
-        Mentorスコア
-      </div>
-
-      <div class="title">
-        15,643
-      </div>
-    </v-card-text> 
+    <card-text-block 
+      title="Mentorスコア"
+      content="15,643" />
 
     <v-divider class="mx-4"></v-divider>  
 
-    <v-card-text>
-      <div class="mb-1 subtitle-1">
-        契約人数
-      </div>
-
-      <div class="title">
-        88人
-      </div>
-    </v-card-text> 
-
+    <card-text-block 
+      title="契約人数"
+      content="88人" />
   </v-card>
 </template>
 
 <script>
-import ButtonOutlined from '../Atoms/ButtonOutlined.vue'
+import CardTextBlock from '../Atoms/TopMentorCard/CardTextBlock.vue'
+import CardSkillBlock from '../Atoms/TopMentorCard/CardSkillBlock.vue'
+import ReviewBlock from '../Atoms/TopMentorCard/ReviewBlock.vue'
 
 export default {
-  components: { ButtonOutlined },
+  components: { CardTextBlock, CardSkillBlock, ReviewBlock },
   props: {
     name: {},
     clickCard: {
@@ -113,6 +62,31 @@ export default {
   data: () => ({
     loading: false,
     selection: 1,
+    skills: [
+      {
+        name: 'First',
+        color: 'green'
+      },
+      {
+        name: 'Second',
+        color: 'red'
+      },
+      {
+        name: 'Third',
+        color: 'orange'
+      },
+      {
+        name: 'Fourth',
+        color: 'blue'
+      },
+      {
+        name: 'Fifth',
+        color: 'black'
+      }
+    ],
+    review: {
+      value: 4.5
+    }
   }),
   methods: {
     reserve () {
